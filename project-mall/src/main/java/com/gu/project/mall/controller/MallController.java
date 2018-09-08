6e448dd62468f1144daae94aabe4f20ec84f497c
GuZhiLong <1828346326@qq.com>
2018-09-08 10:59:14 +0800

    zipkin-完整版

--- project-mall/src/main/java/com/gu/project/mall/controller/MallController.java
+++ project-mall/src/main/java/com/gu/project/mall/controller/MallController.java
@@ -1,5 +1,7 @@
 package com.gu.project.mall.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,14 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MallController {
 
+    public static Logger logger = LoggerFactory.getLogger(MallController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("testSearch")
     public String testSearch(){
+        logger.info("执行了：testSearch方法");
         String data = restTemplate.getForEntity("http://PROJECT-SEARCH/search", String.class).getBody();
         return data;
     }
--- project-search/src/main/java/com/gu/project/search/controller/ProjectSearchController.java
+++ project-search/src/main/java/com/gu/project/search/controller/ProjectSearchController.java
@@ -1,13 +1,18 @@
 package com.gu.project.search.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProjectSearchController {
 
+    public static Logger logger = LoggerFactory.getLogger(ProjectSearchController.class);
+
     @RequestMapping("search")
     public String search(){
+        logger.info("------执行了搜索：search方法");
         String data = "这是第一个SpringCloud项目";
         return data;
     }
